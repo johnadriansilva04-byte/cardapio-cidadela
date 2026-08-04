51d36359d09caf534ebd817c5136185dcc783c77
johnadriansilva04-byte <johnadriansilva04@gmail.com>
2026-08-03 23:44:30 -0300

    Adicionar Google AdSense

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -118,6 +118,24 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXXXXX');
+            `,
+          }}
+        />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2783546143377409"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>
         {children}
